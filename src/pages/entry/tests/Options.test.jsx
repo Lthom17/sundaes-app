52ab18fab9b8242d0371719c5f92857faa8011fd
a1623784 <a1623784@bestbuy.com>
2022-02-18 14:30:03 -0600

    Test and code OrderEntry

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -1,10 +1,10 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '../../../test-utils/testing-library-utils'
 import { OrderDetailsProvider } from '../../../contexts/OrderDetails';
 
 import Options from '../Options'
 
 test('Displays image for each scoop from the server', async () => {
-    render(<Options optionType='scoops' />, { wrapper: OrderDetailsProvider });
+    render(<Options optionType='scoops' />);
 
     const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
     expect(scoopImages).toHaveLength(3);
